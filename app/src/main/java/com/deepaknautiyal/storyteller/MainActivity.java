1cb9f86342a49321a2b60318e8b6de1095481ba2
Deepak Nautiyal <dnautiya@gmail.com>
2015-11-27 19:15:47 +0530

    Process Input name from user

--- app/src/main/java/com/deepaknautiyal/storyteller/MainActivity.java
+++ app/src/main/java/com/deepaknautiyal/storyteller/MainActivity.java
@@ -8,15 +8,35 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mNameField;
+    private Button mStartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        mNameField = (EditText)findViewById(R.id.nameEditText);
+        mStartButton = (Button)findViewById(R.id.startButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mNameField.getText().toString();
+                Toast.makeText(MainActivity.this,name,Toast.LENGTH_LONG).show();
+            }
+        };
+
+        mStartButton.setOnClickListener(listener);
+
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -28,10 +48,10 @@ public class MainActivity extends AppCompatActivity {
         }); */
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        // Added by Deepak to test
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
